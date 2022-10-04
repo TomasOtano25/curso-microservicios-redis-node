e6236ec15d4a95362ee1366d40dcfd5c2fd317b7
Tomas Garcia <tomasotano25@gmail.com>
2022-10-04 10:45:24 -0400

    Clase 11 gestion de permisos

--- config.js
+++ config.js
@@ -2,7 +2,11 @@ require('dotenv').config()
 
 const config = {
   api: {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || 'secreto'
   }
 }
 
